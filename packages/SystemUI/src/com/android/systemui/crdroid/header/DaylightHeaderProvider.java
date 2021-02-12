b6ffffbdf0c25d3a01e4ed0e9d5d5ec45804f997
Shekhar <meshekharbehera613@gmail.com>
2021-02-12 13:32:57 +0530

    la1

--- packages/SystemUI/src/com/android/systemui/crdroid/header/DaylightHeaderProvider.java
+++ packages/SystemUI/src/com/android/systemui/crdroid/header/DaylightHeaderProvider.java
@@ -50,7 +50,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.stag.Utils;
 
 import com.android.systemui.R;
 
